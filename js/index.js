52860ee02435e1cac460dc5c91e50f06037fdac8
mainisameer <68697034+mainisameer@users.noreply.github.com>
2020-07-24 06:58:20 -0700

    update after

--- js/index.js
+++ js/index.js
@@ -1,28 +1,42 @@
 $(document).ready(function(){
     "use strict";
 
-    /*
-    var msg="Hello from Javascript";
-    var resultDiv=document.getElementById("results");
-    console.log(resultDiv);
-    resultDiv.innerHTML="<p>Hello</p>";
-    function showmsg(msg){
-        console.log (msg);
-    }
-    showmsg("this is the message");
-    */
-    var resultList=jQuery("#resultList");
-    resultList.text("change the contents");
-    var toggleButton=$("#toggleButton");
-    toggleButton.on("click",function(){
-        resultList.toggle(500);
-        if(toggleButton.text()=="Hide")
+    
+        
+class FlipFlopGameControl extends Aθεος.Αφροδίτη.UserWorldControl
+{
+    constructor( )
+    {
+        super({
+            Title:"Chaat2"
+            ,ReloadDocumentOnReset: true
+            ,Container:document.getElementById("idMagicUsersContainer")
+        });              
+
+        var sendmessage = this.Server.PatchRaw(function (msg, txt)
         {
-         toggleButton.text("Show");
-        }    
-        else 
-        {
-            toggleButton.text("Hide");
+          //  const user = self.coven.User(msg.sender);
+            //$('#messages').append($('<li>').text(new Date(msg.timestamp) + ":" + user.name + "> " + txt));
+            document.getElementById("output").innerHTML+=txt;
         }
-    })
+        );
+        document.getElementById("sndbtn").addEventListener("click",function (){
+        //document.getElementById("output").innerHTML+=document.getElementById("chatbox").value;
+        sendmessage(document.getElementById("chatbox").value)
+        });
+        
+    }
+    OnInit()
+    {
+        super.OnInit();
+
+    }
+
+   
+}
+
+Aθεος.Αφροδίτη.OnReady().then(()=>new FlipFlopGameControl());
+
+
+
 });
